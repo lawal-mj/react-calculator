90a7b33c2937f2a5d4321a1b1de3930572455541
Abdulmujeeb Lawal <lawalmujeeb78@gmail.com>
2022-06-08 19:23:36 +0100

    add equal to, full stop and 0 to numpad

--- src/components/Numpad.js
+++ src/components/Numpad.js
@@ -1,22 +1,29 @@
-import Button from './blocks/Button'
+import Button from "./blocks/Button";
 
 export default function Numpad() {
   return (
-    <div className="flex bg-orange-400">
-      <div>
-        <Button number={1} />
-        <Button number={4} />
-        <Button number={7} />
+    <div>
+      <div  className="flex bg-orange-400">
+        <div>
+          <Button number={1} />
+          <Button number={4} />
+          <Button number={7} />
+        </div>
+        <div>
+          <Button number={2} />
+          <Button number={5} />
+          <Button number={8} />
+        </div>
+        <div>
+          <Button number={3} />
+          <Button number={6} />
+          <Button number={9} />
+        </div>
       </div>
-      <div>
-        <Button number={2} />
-        <Button number={5} />
-        <Button number={8} />
-      </div>
-      <div>
-        <Button number={3} />
-        <Button number={6} />
-        <Button number={9} />
+      <div  className="flex bg-orange-400 flex-row">
+        <Button number={"."} />
+        <Button number={0} />
+        <Button number={"="} />
       </div>
     </div>
   );
