a1a93936cd043de3b5ff2645d95c22268234e476
Abdulmujeeb Lawal <lawalmujeeb78@gmail.com>
2022-06-08 16:06:14 +0100

    add input box

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import Numpad from "./Numpad"
 import Signpad from "./Signpad"
+import InputBox from "./InputBox"
 
 export default function App(){
     return (
         <div>
+            <InputBox />
             <Signpad />
             <Numpad />
         </div>
---
+++ src/InputBox.js
@@ -0,0 +1,5 @@
+export default function InputBox(){
+    return (
+            <input className='w-48 bg-white border border-red-700' type="text" />
+    )
+}
