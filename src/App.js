616d2ecfdad47bb7842d18105e62aedd510c45d4
Abdulmujeeb Lawal <lawalmujeeb78@gmail.com>
2022-06-08 08:24:08 +0100

    add new sign padd to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
 import Numpad from "./Numpad"
+import Signpad from "./Signpad"
 
 export default function App(){
     return (
         <div>
+            <Signpad />
             <Numpad />
         </div>
     )
