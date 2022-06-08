7cfa26c3628bf3d8fac23296dd783e533844ebb3
Abdulmujeeb Lawal <lawalmujeeb78@gmail.com>
2022-06-08 08:23:33 +0100

    add a component for the signs

---
+++ src/Signpad.js
@@ -0,0 +1,12 @@
+import Sign from "./Sign"
+
+export default function Signpad(){
+    return(
+        <div className="flex">
+            <Sign />
+            <Sign />
+            <Sign />
+            <Sign />
+        </div>
+    )
+}
