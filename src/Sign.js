7b404e536737235ef22ab17c65bc700c79efb0de
Abdulmujeeb Lawal <lawalmujeeb78@gmail.com>
2022-06-08 08:23:10 +0100

    create sign component

---
+++ src/Sign.js
@@ -0,0 +1,10 @@
+export default function Sign() {
+    return (
+      <div>
+        <div className="bg-blue-400 w-12 h-6 flex pb-1 pt-0.5 justify-center items-center text-white border text-base">
+          +
+        </div>
+      </div>
+    );
+  }
+  
