288e4c9828a9f3942652a0ae68ec18a02fdc00e9
Abdulmujeeb Lawal <lawalmujeeb78@gmail.com>
2022-06-09 07:07:44 +0100

    add button role to numpad and sign pad

--- src/components/blocks/Sign.js
+++ src/components/blocks/Sign.js
@@ -1,7 +1,7 @@
 export default function Sign(props) {
     return (
       <div>
-        <div className="bg-blue-400 w-12 h-8 flex pb-1 pt-0.5 justify-center items-center text-white border text-base border-orange-400">
+        <div role="button" className="bg-blue-400 w-12 h-8 flex pb-1 pt-0.5 justify-center items-center text-white border text-base border-orange-400">
          {props.sign}
         </div>
       </div>
