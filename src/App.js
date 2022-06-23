fc5af7c2966fe57cf9f4b61ceb18077769066d1b
Abdulmujeeb Lawal <lawalmujeeb78@gmail.com>
2022-06-24 00:32:46 +0100

    got basic design structure ready

--- src/App.js
+++ src/App.js
@@ -1,34 +1,10 @@
-import { useState } from "react";
+import Numpad from "./Numpad"
 
-import Numpad from "./components/Numpad";
-import Signpad from "./components/Signpad";
-import InputBox from "./components/InputBox";
+export default function App(){
+    return (
+        <div>
+            <Numpad />
+        </div>
 
-export default function App() {
-  const [inputBoxValue, setinputBoxValue] = useState();
-
-  function updateInput(e) {
-    setinputBoxValue(e.target.value);
-  }
-
-
-  function updateNumber(item) {
-    console.log(item);
-    console.log("Clicking");
-  }
-
-  function signEffect(item){
-      console.log("Clicking sign " + item)
-  }
-
-
-  console.log(inputBoxValue);
-  return (
-    <div className="inline-block border-2 border-orange-400 bg-orange-400 mt-9">
-      <InputBox print={updateInput} input={inputBoxValue} />
-      <Signpad sign={signEffect} />
-      <Numpad update={updateNumber} sign={signEffect} />
-
-    </div>
-  );
+    )
 }
---
+++ src/Numpad.js
@@ -0,0 +1,42 @@
+export default function Numpad(){
+    return (
+        <div className="flex flex-col">
+
+            <div className="flex w-72 h-10 flex-auto">
+                <div className="bg-blue-700 grow flex items-center justify-center border">+</div>
+                <div className="bg-blue-700 grow flex items-center justify-center border">-</div>
+                <div className="bg-blue-700 grow flex items-center justify-center border">*</div>
+                <div className="bg-blue-700 grow flex items-center justify-center border">/</div>
+            </div>
+
+            <div className="flex">
+                <div className="w-24 h-14 bg-orange-700 flex items-center justify-center border">1</div>
+                <div className="w-24 h-14 bg-orange-700 flex items-center justify-center border">2</div>
+                <div className="w-24 h-14 bg-orange-700 flex items-center justify-center border">3</div>
+            </div>
+
+
+            <div className="flex">
+                <div className="w-24 h-14 bg-orange-700 flex items-center justify-center border">4</div>
+                <div className="w-24 h-14 bg-orange-700 flex items-center justify-center border">5</div>
+                <div className="w-24 h-14 bg-orange-700 flex items-center justify-center border">6</div>
+            </div>
+
+
+            <div className="flex">
+                <div className="w-24 h-14 bg-orange-700 flex items-center justify-center border">7</div>
+                <div className="w-24 h-14 bg-orange-700 flex items-center justify-center border">8</div>
+                <div className="w-24 h-14 bg-orange-700 flex items-center justify-center border">9</div>
+            </div>
+
+
+            <div className="flex">
+                <div className="w-24 h-12 bg-yellow-400 flex items-center justify-center border">.</div>
+                <div className="w-24 h-12 bg-orange-700 flex items-center justify-center border">0</div>
+                <div className="w-24 h-12 bg-purple-700 flex items-center justify-center border">=</div>
+            </div>
+
+        </div>
+    )
+
+}
