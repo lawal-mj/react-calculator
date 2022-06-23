fc6cd610e4da98c7546c5242b3cc2b060212fb46
Abdulmujeeb Lawal <lawalmujeeb78@gmail.com>
2022-06-24 00:48:19 +0100

    add outline

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Numpad from "./Numpad"
+import InputBox from "./InputBox"
 
 export default function App(){
     return (
-        <div>
+        <div className="mt-10 ml-10 border-4 border-black inline-block">
+            <InputBox />
             <Numpad />
         </div>
 
