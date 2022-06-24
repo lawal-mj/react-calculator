228567047d07f827e54f41802c43b66b2f1ed1b5
Abdulmujeeb Lawal <lawalmujeeb78@gmail.com>
2022-06-24 10:38:54 +0100

    replaced input box with div

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
-import Numpad from "./Numpad"
-import InputBox from "./InputBox"
-import { useState } from "react"
+import Numpad from "./Numpad";
+import InputBox from "./InputBox";
+import { useState } from "react";
 
-export default function App(){
+export default function App() {
+  const [inputValue, setInputValue] = useState(0);
 
-    const [ inputValue, setInputValue ] = useState();
+  function updateInputBox(e) {
+    setInputValue(e.target.value);
+  }
 
-    function updateInputBox(e){
-        setInputValue(e.target.value)
-    }
+  function handleNumber(e) {
+    setInputValue(inputValue + e.target.textContent);
+  }
 
-    function handleNumber(){
-        console.log("This is a number")
-    }
+  function handleSign() {
+    console.log("This is a SIGN bro");
+  }
 
-    function handleSign(){
-        console.log("This is a SIGN bro")
-    }
+  console.log(inputValue)
+  return (
+    <div className="mt-10 ml-10 border-4 border-black inline-block">
+      <InputBox value={inputValue} updateInput={updateInputBox} />
 
-
-    // console.log(inputValue)
-    return (
-        <div className="mt-10 ml-10 border-4 border-black inline-block">
-            <InputBox updateInput={updateInputBox}/>
-
-            <Numpad numberFunction={handleNumber} signFunction={handleSign}/>
-        </div>
-
-    )
+      <Numpad numberFunction={handleNumber} signFunction={handleSign} />
+    </div>
+  );
 }
--- src/InputBox.js
+++ src/InputBox.js
@@ -1,7 +1,9 @@
-export default function InputBox(props){
-    return (
-        <div>
-            <input onChange={props.updateInput} type="text" className="w-72 h-12 indent-2 border border-red-700" />
-        </div>
-    )
+export default function InputBox(props) {
+  return (
+    <div onChange={()=>props.updateInput()} className="w-72 h-12 indent-2 border border-red-700 items-center flex justify-center text-xl">  
+        {props.value}
+    </div>
+  )
 }
+
+// 
