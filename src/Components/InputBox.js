2a7d0149fc2892794a689eb9c46343be117da1f5
Abdulmujeeb Lawal <lawalmujeeb78@gmail.com>
2022-06-26 20:04:51 +0100

    reduced indent on the input box

--- src/Components/InputBox.js
+++ src/Components/InputBox.js
@@ -1,6 +1,6 @@
 export default function InputBox(props){
     return (
-        <div className="bg-purple-300 h-20 indent-7 text-2xl pt-4 rounded-t-2xl">
+        <div className="bg-purple-300 h-20 indent-6 text-2xl pt-4 rounded-t-2xl">
             {props.input}
         </div>
     )
