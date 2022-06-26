08aa66a65a650dd8f7b657da55883e5497b4b8b9
Abdulmujeeb Lawal <lawalmujeeb78@gmail.com>
2022-06-26 01:52:12 +0100

    Add border radius to top

--- src/Components/InputBox.js
+++ src/Components/InputBox.js
@@ -1,6 +1,6 @@
 export default function InputBox(props){
     return (
-        <div>
+        <div className="bg-purple-300 h-10 indent-5 text-lg pt-1 rounded-t-2xl">
             {props.input}
         </div>
     )
